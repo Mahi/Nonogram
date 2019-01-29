a7c7704ab866e4cc6e84295af3705ca5263973f9
Markus Meskanen <markusmeskanen@gmail.com>
2019-01-29 15:02:53 +0200

    Initial project skeleton with MainWindow

---
+++ mainwindow.cpp
@@ -0,0 +1,6 @@
+#include "mainwindow.hh"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent)
+{
+}
---
+++ mainwindow.hh
@@ -0,0 +1,14 @@
+#ifndef MAINWINDOW_HH
+#define MAINWINDOW_HH
+
+#include <QMainWindow>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+};
+
+#endif // MAINWINDOW_HH
