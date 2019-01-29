2669e24999ebd4b09bcb0bc883565465aa92b5df
Markus Meskanen <markusmeskanen@gmail.com>
2019-01-29 15:18:50 +0200

    Menu bar with open map action

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,15 @@
+#include <QMenu>
+#include <QMenuBar>
+#include <QAction>
+
 #include "mainwindow.hh"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent)
 {
+    QMenu *fileMenu = new QMenu("File");
+    menuBar()->addMenu(fileMenu);
+
+    QAction *openMapAction = new QAction("Open map...");
+    fileMenu->addAction(openMapAction);
 }
